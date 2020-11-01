5c09bdeb24946b8f3cec567b829469258b864781
Thiago Caiado <thiago.caiado@hotmail.com>
2020-11-01 07:18:21 -0300

    camerax upgrade dependencies / camera view dependency

---
+++ app/src/main/java/com/example/cameraapp/camerax/CameraxActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cameraapp.camerax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cameraapp.R
+
+class CameraxActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camerax)
+    }
+}
